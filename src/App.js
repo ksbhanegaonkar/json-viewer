df83700ca9e3c25ba2ff4ccee823c9fd4f69a43a
Kedar Bhanegaonkar <kedarsbhanegaonkar@gmail.com>
2021-01-09 11:16:34 +0530

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
+import Flexbox from 'flexbox-react';
+import React from 'react';
 import './App.css';
 
-function App() {
+class App extends React.Component {
+
+  render(){
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Flexbox>test</Flexbox>
+        <Flexbox>test1</Flexbox>
+        <Flexbox>test2</Flexbox>
     </div>
   );
+  }
 }
 
 export default App;
